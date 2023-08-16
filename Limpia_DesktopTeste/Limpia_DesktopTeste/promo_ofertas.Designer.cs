cc3dc2af1b60cd45683d89e3f88cb88dca5a547b
girafasmarinhas <132388153+girafasmarinhas@users.noreply.github.com>
2023-08-16 12:27:10 -0300

    15/08

--- Limpia_DesktopTeste/Limpia_DesktopTeste/promo_ofertas.Designer.cs
+++ Limpia_DesktopTeste/Limpia_DesktopTeste/promo_ofertas.Designer.cs
@@ -28,14 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Location = new System.Drawing.Point(36, 57);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(221, 123);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel2.Location = new System.Drawing.Point(304, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(221, 123);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel3.Location = new System.Drawing.Point(561, 57);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(221, 123);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel4.Location = new System.Drawing.Point(561, 217);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(221, 123);
+            this.panel4.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel5.Location = new System.Drawing.Point(304, 217);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(221, 123);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel6.Location = new System.Drawing.Point(36, 217);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(221, 123);
+            this.panel6.TabIndex = 3;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel7.Location = new System.Drawing.Point(561, 383);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(221, 123);
+            this.panel7.TabIndex = 5;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel8.Location = new System.Drawing.Point(304, 383);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(221, 123);
+            this.panel8.TabIndex = 4;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel9.Location = new System.Drawing.Point(36, 383);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(221, 123);
+            this.panel9.TabIndex = 3;
             // 
             // promo_ofertas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1104, 726);
+            this.ClientSize = new System.Drawing.Size(828, 590);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "promo_ofertas";
             this.Text = "promo_ofertas";
             this.ResumeLayout(false);
@@ -43,5 +134,15 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
     }
 }
