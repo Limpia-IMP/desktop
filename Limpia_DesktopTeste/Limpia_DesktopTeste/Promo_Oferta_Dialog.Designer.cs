2bbbf9af73a34571a8e87d1c7141cc359238a5b1
ianbatman <ian.lucania@etec.sp.gov.br>
2023-11-16 19:07:06 -0300

    ultimo uptade acho

--- Limpia_DesktopTeste/Limpia_DesktopTeste/Promo_Oferta_Dialog.Designer.cs
+++ Limpia_DesktopTeste/Limpia_DesktopTeste/Promo_Oferta_Dialog.Designer.cs
@@ -66,9 +66,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(515, 40);
+            this.panel1.Size = new System.Drawing.Size(828, 40);
             this.panel1.TabIndex = 0;
             // 
             // btnVoltar
@@ -77,7 +77,7 @@
             this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
             this.btnVoltar.Location = new System.Drawing.Point(4, 2);
-            this.btnVoltar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(2);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(34, 37);
             this.btnVoltar.TabIndex = 3;
@@ -104,7 +104,7 @@
             this.label2.Location = new System.Drawing.Point(28, 52);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.Size = new System.Drawing.Size(108, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Resumo da Oferta:";
             // 
@@ -116,7 +116,7 @@
             this.label3.Location = new System.Drawing.Point(28, 145);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.Size = new System.Drawing.Size(45, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Título: ";
             // 
@@ -128,7 +128,7 @@
             this.label4.Location = new System.Drawing.Point(28, 180);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 16);
+            this.label4.Size = new System.Drawing.Size(112, 16);
             this.label4.TabIndex = 5;
             this.label4.Text = "Disponível durante:";
             // 
@@ -159,7 +159,7 @@
             this.label5.Location = new System.Drawing.Point(199, 182);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 16);
+            this.label5.Size = new System.Drawing.Size(12, 16);
             this.label5.TabIndex = 8;
             this.label5.Text = "-";
             // 
@@ -171,7 +171,7 @@
             this.label6.Location = new System.Drawing.Point(28, 217);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.Size = new System.Drawing.Size(50, 16);
             this.label6.TabIndex = 9;
             this.label6.Text = "Público:";
             // 
@@ -230,7 +230,7 @@
             this.label7.Location = new System.Drawing.Point(28, 257);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(77, 16);
+            this.label7.Size = new System.Drawing.Size(76, 16);
             this.label7.TabIndex = 11;
             this.label7.Text = "Responsável:";
             // 
@@ -242,7 +242,7 @@
             this.label8.Location = new System.Drawing.Point(28, 288);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(49, 16);
+            this.label8.Size = new System.Drawing.Size(48, 16);
             this.label8.TabIndex = 13;
             this.label8.Text = "Código:";
             // 
@@ -254,7 +254,7 @@
             this.label9.Location = new System.Drawing.Point(28, 320);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(48, 16);
+            this.label9.Size = new System.Drawing.Size(47, 16);
             this.label9.TabIndex = 15;
             this.label9.Text = "Região:";
             // 
@@ -309,7 +309,7 @@
             // 
             this.pictureBox1.BackgroundImage = global::Limpia_DesktopTeste.Properties.Resources.banheiro;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(303, 71);
+            this.pictureBox1.Location = new System.Drawing.Point(585, 102);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(200, 134);
@@ -327,7 +327,7 @@
             this.btnEditarPromo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEditarPromo.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditarPromo.ForeColor = System.Drawing.Color.Black;
-            this.btnEditarPromo.Location = new System.Drawing.Point(303, 218);
+            this.btnEditarPromo.Location = new System.Drawing.Point(585, 249);
             this.btnEditarPromo.Margin = new System.Windows.Forms.Padding(2);
             this.btnEditarPromo.Name = "btnEditarPromo";
             this.btnEditarPromo.Size = new System.Drawing.Size(86, 32);
@@ -347,7 +347,7 @@
             this.btnInativarPromo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInativarPromo.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInativarPromo.ForeColor = System.Drawing.Color.Black;
-            this.btnInativarPromo.Location = new System.Drawing.Point(417, 218);
+            this.btnInativarPromo.Location = new System.Drawing.Point(699, 249);
             this.btnInativarPromo.Margin = new System.Windows.Forms.Padding(2);
             this.btnInativarPromo.Name = "btnInativarPromo";
             this.btnInativarPromo.Size = new System.Drawing.Size(86, 32);
@@ -360,7 +360,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 362);
+            this.ClientSize = new System.Drawing.Size(828, 590);
             this.Controls.Add(this.btnEditarPromo);
             this.Controls.Add(this.btnInativarPromo);
             this.Controls.Add(this.txtCodigoPromo);
@@ -382,7 +382,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Promo_Oferta_Dialog";
             this.Text = "Promo_Oferta_Dialog";
             this.panel1.ResumeLayout(false);
