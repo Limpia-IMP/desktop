20caa749db85dab581824485446e40c0aa56c22d
girafasmarinhas <132388153+girafasmarinhas@users.noreply.github.com>
2023-11-25 20:17:03 -0300

    25/11🌟
    pág personalizada de cursos e rounded picturebox

--- Limpia_DesktopTeste/Limpia_DesktopTeste/adm_avisos.Designer.cs
+++ Limpia_DesktopTeste/Limpia_DesktopTeste/adm_avisos.Designer.cs
@@ -28,11 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adm_avisos));
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.roundPictureBox1 = new Limpia_DesktopTeste.RoundPictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnResolvido = new Limpia_DesktopTeste.BtnLimpia.btnPrincipal();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.roundPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox3
@@ -64,10 +74,90 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panel1.Location = new System.Drawing.Point(105, 101);
+            this.panel1.Controls.Add(this.btnResolvido);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.roundPictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(98, 77);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(907, 537);
+            this.panel1.Size = new System.Drawing.Size(899, 580);
             this.panel1.TabIndex = 53;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.btnVoltar);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(899, 60);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.FlatAppearance.BorderSize = 0;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("btnVoltar.Image")));
+            this.btnVoltar.Location = new System.Drawing.Point(11, 7);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(51, 46);
+            this.btnVoltar.TabIndex = 2;
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(292, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Atualização de Segurança";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // roundPictureBox1
+            // 
+            this.roundPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("roundPictureBox1.Image")));
+            this.roundPictureBox1.Location = new System.Drawing.Point(359, 95);
+            this.roundPictureBox1.Name = "roundPictureBox1";
+            this.roundPictureBox1.Size = new System.Drawing.Size(205, 205);
+            this.roundPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.roundPictureBox1.TabIndex = 8;
+            this.roundPictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(847, 101);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Ah cas" +
+    "o você esteja se perguntando sobre a imagem, só usei uma imagem aleatória pra mo" +
+    "strar que tá funcionando.\r\n\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnResolvido
+            // 
+            this.btnResolvido.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnResolvido.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnResolvido.BorderColor = System.Drawing.Color.Aqua;
+            this.btnResolvido.BorderRadius = 40;
+            this.btnResolvido.BorderSize = 0;
+            this.btnResolvido.FlatAppearance.BorderSize = 0;
+            this.btnResolvido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResolvido.Font = new System.Drawing.Font("Berlin Sans FB", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResolvido.ForeColor = System.Drawing.Color.White;
+            this.btnResolvido.Location = new System.Drawing.Point(359, 462);
+            this.btnResolvido.Name = "btnResolvido";
+            this.btnResolvido.Size = new System.Drawing.Size(205, 50);
+            this.btnResolvido.TabIndex = 11;
+            this.btnResolvido.Text = "Resolvido";
+            this.btnResolvido.TextColor = System.Drawing.Color.White;
+            this.btnResolvido.UseVisualStyleBackColor = false;
             // 
             // adm_avisos
             // 
@@ -80,8 +170,13 @@
             this.Controls.Add(this.pictureBox3);
             this.Name = "adm_avisos";
             this.Text = "adm_avisos";
+            this.Load += new System.EventHandler(this.adm_avisos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.roundPictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -91,5 +186,11 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Label label1;
+        private RoundPictureBox roundPictureBox1;
+        private System.Windows.Forms.Label label2;
+        private BtnLimpia.btnPrincipal btnResolvido;
     }
 }
--- Limpia_DesktopTeste/Limpia_DesktopTeste/adm_avisos.cs
+++ Limpia_DesktopTeste/Limpia_DesktopTeste/adm_avisos.cs
@@ -16,5 +16,24 @@ namespace Limpia_DesktopTeste
         {
             InitializeComponent();
         }
+
+        private void adm_avisos_Load(object sender, EventArgs e)
+        {
+            SetRoundedEdges(panel1, 30);
+        }
+        public void SetRoundedEdges(Panel panel, int radius)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.StartFigure();
+            path.AddArc(new Rectangle(0, 0, radius, radius), 180, 90);
+            path.AddLine(radius, 0, panel.Width - radius, 0);
+            path.AddArc(new Rectangle(panel.Width - radius, 0, radius, radius), 270, 90);
+            path.AddLine(panel.Width, radius, panel.Width, panel.Height - radius);
+            path.AddArc(new Rectangle(panel.Width - radius, panel.Height - radius, radius, radius), 0, 90);
+            path.AddLine(panel.Width - radius, panel.Height, radius, panel.Height);
+            path.AddArc(new Rectangle(0, panel.Height - radius, radius, radius), 90, 90);
+            path.CloseFigure();
+            panel.Region = new System.Drawing.Region(path);
+        }
     }
 }
